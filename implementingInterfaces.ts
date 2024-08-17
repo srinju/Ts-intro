ebfb9a46d624f4bad229c46e82cbe1bb8b8887aa
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-08-18 02:15:39 +0530

    Create implementingInterfaces.ts

---
+++ implementingInterfaces.ts
@@ -0,0 +1,26 @@
+
+
+interface Person {
+    name : string,
+    age : number,
+    greet(phrase : string) : void
+}
+
+//class implemneting the above interface  >>
+
+class Employee implements Person {
+    name : string; //we are defing the name and age here coz we need to do this inside classes before the constructior coz we are gonna use it at last as a object see the new employee line at last
+    age : number;
+
+    constructor(n : string , a : number) {
+        this.name = n; //in the constructor we will define it what will the object have and this.name it to the things the object are gonna contain
+        this.age = a;
+    }
+
+    greet(phrase: string): void {
+        console.log(`${phrase} ${this.name}`);
+    }
+}
+
+const e = new Employee("harkirat" , 22);
+console.log(e);
